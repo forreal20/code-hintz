1028532146d7f6777e7a2fbfe3b4085d2031ac8e
Tajir Ramadhan <111126403+Tajir-Ramadhan@users.noreply.github.com>
2025-05-21 20:47:43 +0300

    Thanks for the invitation, here is your message

---
+++ maoni.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+  printf("Hello brother, its pleassuere to meet you here \n");
+
+  printf("Unfortunately you failed to meet the deadline we have set. \n");
+
+  printf("\n\n\n\n");
+
+  printf(" **** MAKE SURE YOU PRACTISE ALOT TO GAIN SUPER MOMENTUM ****");
+}
